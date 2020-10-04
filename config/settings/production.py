445b308bd4377132c03c3af4de13db3c2d47ca28
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-04 09:05:36 +0200

    added site to allowed host

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,6 @@ import logging  # noqa
 from .base import *  # noqa
 from .base import env  # noqa
 
-ALLOWED_HOSTS = ["django-cast.com"]
+ALLOWED_HOSTS = ["django-cast.com", "wersdoerfer.de"]
 
 ADMIN_URL = "hidden_admin/"
