fd63767f7b1538bccfa40ea03ef40983bf8cd785
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-04 14:54:14 +0200

    application server should not be accessible from outside

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gunicorn_path=$(find /home/cast/.cache/pypoetry/virtualenvs -name "gunicorn" | grep -v lib)
-${gunicorn_path} -b :8001 config.wsgi
+${gunicorn_path} -b 127.0.0.1:8001 config.wsgi
