a25742817505cc2d2513e72b8e6f1030ab909da4
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-04 09:06:39 +0200

    host hast to be exact?

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,6 @@ import logging  # noqa
 from .base import *  # noqa
 from .base import env  # noqa
 
-ALLOWED_HOSTS = ["django-cast.com", "wersdoerfer.de"]
+ALLOWED_HOSTS = ["django-cast.com", "staging.wersdoerfer.de"]
 
 ADMIN_URL = "hidden_admin/"
