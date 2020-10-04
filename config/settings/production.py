2202007a42697127a70ac030557777e83f8bc090
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-04 09:08:19 +0200

    added python-podcast..

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,6 @@ import logging  # noqa
 from .base import *  # noqa
 from .base import env  # noqa
 
-ALLOWED_HOSTS = ["django-cast.com", "staging.wersdoerfer.de"]
+ALLOWED_HOSTS = ["django-cast.com", "staging.wersdoerfer.de", "staging.python-podcast.de"]
 
 ADMIN_URL = "hidden_admin/"
