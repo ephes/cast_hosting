548b9f2e02c4e3aec4c61a0cdce36819532a4434
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-09 19:23:52 +0200

    added django-extensions

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,3 +2,8 @@ from .base import *  # noqa
 from .base import env  # noqa
 
 DEBUG = True
+
+# django-extensions
+# ------------------------------------------------------------------------------
+# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
+INSTALLED_APPS += ["django_extensions"]  # noqa F405
