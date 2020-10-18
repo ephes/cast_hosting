3f0fd62259d9691f95aa8aa7675f902ee4717430
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-18 09:24:44 +0200

    added threadedcomments urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -16,6 +16,8 @@ urlpatterns = [
     # User management
     path("users/", include("apps.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
+    # Threadedcomments
+    re_path(r"^show/comments/", include("fluent_comments.urls")),
     # Wagtail
     path("cms/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
