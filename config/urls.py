651d56fdca5f6e906a7e93ca62afdf7690ded57b
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-25 14:35:37 +0100

    urls

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,7 @@ from apps.views import test_site
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("site/", test_site, name="test-site"),
+    # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("apps.users.urls", namespace="users")),
@@ -19,7 +20,8 @@ urlpatterns = [
     # Threadedcomments
     re_path(r"^show/comments/", include("fluent_comments.urls")),
     # Wagtail
-    path("cms/", include(wagtailadmin_urls)),
-    path("documents/", include(wagtaildocs_urls)),
+    # Wagtail
+    url(r"^cms/", include(wagtailadmin_urls)),
+    url(r"^documents/", include(wagtaildocs_urls)),
     re_path(r"", include(wagtail_urls)),  # default is wagtail
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
