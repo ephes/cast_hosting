01117e7de139775ce7c06354d5e862ff55200efb
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-25 14:37:58 +0100

    stderr and stdout handling

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gunicorn_path=$(find /home/cast/.cache/pypoetry/virtualenvs -name "gunicorn" | grep -v lib)
 cd /home/cast/site
-${gunicorn_path} -b 127.0.0.1:8001 config.wsgi
+${gunicorn_path} --access-logfile - --error-logfile - -b 127.0.0.1:8001 config.wsgi
