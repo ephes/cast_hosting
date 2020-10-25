a06d11a2c68519fe657a9f14f81e003831dce0ab
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-10-25 14:43:49 +0100

    forgot to import url

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, url, path, re_path
 from django.views.generic import TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -19,7 +19,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     # Threadedcomments
     re_path(r"^show/comments/", include("fluent_comments.urls")),
-    # Wagtail
     # Wagtail
     url(r"^cms/", include(wagtailadmin_urls)),
     url(r"^documents/", include(wagtaildocs_urls)),
