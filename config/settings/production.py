62df452319ad9cfd447ef4d3b74e525b1b3991b3
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-06-29 19:14:38 +0200

    there has to be a LOGGING setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,3 +6,18 @@ from .base import env  # noqa
 ALLOWED_HOSTS = ["django-cast.com", "staging.wersdoerfer.de", "staging.python-podcast.de"]
 
 ADMIN_URL = "hidden_admin/"
+
+# There has to be a LOGGING setting otherwise we get a 500, dunno why
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
