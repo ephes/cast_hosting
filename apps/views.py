ad6ba481f3c4cbe207c5af2bb0efacd2a46582ef
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-01-01 19:42:11 +0100

    determine current site properly

--- apps/views.py
+++ apps/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+
 
 
 def test_site(request):
-    return render(request, "pages/site.html")
+    print("request: ", request)
+    current_site = get_current_site(request)
+    print("current site: ", current_site)
+    context = {"current_site": current_site}
+    return render(request, "pages/site.html", context=context)
 
 
 def hello(request):
